a7bda4ab834d8f3e0cefd356b1b70a209a29e4e2
Mirza Faris Bin Mohd Affandi <156393988+MirzaFaris12@users.noreply.github.com>
2025-07-06 14:09:29 -0400

    Update insider_scraper.py

--- insider_scraper.py
+++ insider_scraper.py
@@ -6,7 +6,7 @@ class InsiderScraper:
     
     def __init__(self, min_transaction_value=0, min_shares=0):
         self.base_url = "http://openinsider.com"
-        self.url = f"{self.base_url}/screener?fd=3&td=0&nop=50"
+        self.url = f"{self.base_url}/screener?s=&o=&pl=&ph=&ll=&lh=&fd=30&td=0&sic1=&sic2=&t=&ql=&qh=&o1=0&o2=0&nop=50"
         self.min_transaction_value = min_transaction_value
         self.min_shares = min_shares
 
